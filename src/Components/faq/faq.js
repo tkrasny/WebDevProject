9c366560e890c53dde0aad93c04f0bfbfc1a3d55
Brian Cariddi <bcariddi@nd.edu>
2021-03-29 23:47:42 -0400

    faq text

--- src/Components/faq/faq.js
+++ src/Components/faq/faq.js
@@ -5,6 +5,15 @@ export function FAQ() {
     return (
         <div>
             <h1>FAQ</h1>
+            <h2>Basics</h2>
+            <h4>What's the difference between betting on the spread and the money line?</h4>
+            <p>Betting on the spread means betting on the number of points a team is going to win or lose by. For
+            example, if you bet on the Giants +6.5 against the Steelers, you are wagering that the Giants will lose by
+            6 points or less or win the game. If you bet on the Steelers -6.5 to beat the Giants, you are betting
+            on the Steelers to win by at least 7 points. Betting on the money line simply means betting on who is going
+            to win a game outright. The key difference here is that you would get better odds betting on the Giants
+            because they are the underdog in the game, and betting on the Steelers money line would give you less value
+            because they are heavy favorites.</p>
         </div>
     );
 }
