776c16c214ba4c6d22517a0cb33fe503b352f363
Tyler Krasny <tekrasny@gmail.om>
2021-05-09 00:57:36 -0400

    CSS good

--- src/Components/homepage/homepage.js
+++ src/Components/homepage/homepage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./homepage.css";
 
-import analysisImg from "./analysis1.jpg"
+import analysisImg from "./analysis1.png"
 import scheduleImg from "./calendar.png"
 import faqImg from "./question.png"
 
@@ -31,28 +31,28 @@ export function Homepage() {
     {/* Displays the main body of the home page as a css grid*/}
     return (
     <main id="homepage">
-        <div class="container container_1">
-            <button onClick={onClickAnalysis} class="myButton">Analysis & Recommendations</button>
+        <div className="container container_1">
+            <button onClick={onClickAnalysis} className="myButton" >Analysis & Recommendations</button>
         </div>
-        <div class="container container_2">
+        <div className="container container_2">
             <figure>
-                <img src={analysisImg} class="container" alt="analysis"/>
+                <img src={analysisImg} className="container" alt="analysis" style={{height: 400, width: "auto"}}/>
             </figure>
         </div>
-        <div class="container container_3">
+        <div className="container container_3">
             <figure>
-                <img src={scheduleImg} class="container" alt="calendar"/>
+                <img src={scheduleImg} className="container" alt="calendar" style={{height: 400, width: "auto"}}/>
             </figure>
         </div>
-        <div class="container container_4">
-            <button onClick={onClickSchedule} class="myButton">Schedule</button>
+        <div className="container container_4">
+            <button onClick={onClickSchedule} className="myButton">Schedule</button>
         </div>
-        <div class="container container_5">
-            <button onClick={onClickFAQ} class="myButton">Frequently Asked Questions</button>
+        <div className="container container_5">
+            <button onClick={onClickFAQ} className="myButton" >Frequently Asked Questions</button>
         </div>
-        <div class="container container_6">
+        <div className="container container_6">
             <figure>
-                <img src={faqImg} class="container" alt="faq"/>
+                <img src={faqImg} className="container" alt="faq" style={{height: 400, width: "auto"}}/>
             </figure>
         </div>
     </main>
