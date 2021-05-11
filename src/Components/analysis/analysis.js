0efd293f9638824adb165aa20d87bb754f751fbd
Tyler Krasny <tekrasny@gmail.om>
2021-05-11 01:03:32 -0400

    Data guys

--- src/Components/analysis/analysis.js
+++ src/Components/analysis/analysis.js
@@ -1,56 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./analysis.css";
+import { BarChart } from "../../Common/Services/DataVisualization/datavis";
+import Parse from "parse";
 
 export function Analysis() {
 
-    // TODO: retrieve real data for this instead of hard coding
+    const [rows, setRows] = useState([]);
+
+    useEffect(() => {
+        const NbaTeamStatsObj = Parse.Object.extend("NBATeamStats");
+        const query = new Parse.Query(NbaTeamStatsObj)
+        query.find().then((objects) => {
+            objects.forEach((row) => {
+                const OREB = row.get("OREB");
+                const BLKA = row.get("BLKA");
+                const FGA = row.get("FGA");
+                const BLK = row.get("BLK");
+                const AST = row.get("AST");
+                const FG_PCT = row.get("FG_PCT");
+                const PLUS_MINUS = row.get("PLUS_MINUS");
+                const DREB = row.get("DREB");
+                const STL = row.get("STL");
+                const L = row.get("L");
+                const FGM = row.get("FGM");
+                const PFD = row.get("PFD");
+                const REB = row.get("REB");
+                const PTS = row.get("PTS");
+                const FT_PCT = row.get("FT_PCT");
+                const FTA = row.get("FTA");
+                const FG3_PCT = row.get("FG3_PCT");
+                const PF = row.get("PF");
+                const W = row.get("W");
+                const W_PCT = row.get("W_PCT");
+                const TOV = row.get("TOV");
+                const FG3M = row.get("FG3M");
+                const FTM = row.get("FTM");
+                const TeamName = row.get("TeamName");
+                const TeamID = row.get("TeamID");
+
+                const rowJson = {
+                    "OREB": OREB,
+                    "BLKA": BLKA,
+                    "FGA": FGA,
+                    "BLK": BLK,
+                    "AST": AST,
+                    "FG_PCT": FG_PCT,
+                    "PLUS_MINUS": PLUS_MINUS,
+                    "DREB": DREB,
+                    "STL": STL,
+                    "L": L,
+                    "FGM": FGM,
+                    "PFD": PFD,
+                    "REB": REB,
+                    "PTS": PTS,
+                    "FT_PCT": FT_PCT,
+                    "FTA": FTA,
+                    "FG3_PCT": FG3_PCT,
+                    "PF": PF,
+                    "W": W,
+                    "W_PCT": W_PCT,
+                    "TOV": TOV,
+                    "FG3M": FG3M,
+                    "FTM": FTM,
+                    "TeamName": TeamName,
+                    "TeamID": TeamID,
+                }
+                setRows(rows => [...rows, rowJson])
+            })
+        })
+    }, [])
+
     return (
         <div>
-            <h1>Analysis</h1>
-            <h3>Top 5 NBA Over Rates</h3>
-            <table id="nbaovers" className="styled-table">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Team</th>
-                    <th>Over Record</th>
-                    <th>Over %</th>
-                </tr>
-                </thead>
-
-                <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>New Orleans</td>
-                    <td>30-16-0</td>
-                    <td>65.2%</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Brooklyn</td>
-                    <td>29-18-0</td>
-                    <td>61.7%</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Indiana</td>
-                    <td>27-17-1</td>
-                    <td>61.4%</td>
-                </tr>
-                <tr>
-                    <td>4</td>
-                    <td>Milwaukee</td>
-                    <td>27-17-1</td>
-                    <td>61.4%</td>
-                </tr>
-                <tr>
-                    <td>5</td>
-                    <td>Denver</td>
-                    <td>28-18-0</td>
-                    <td>60.9%</td>
-                </tr>
-                </tbody>
-            </table>
+            <BarChart data={rows} />
         </div>
     );
 }
